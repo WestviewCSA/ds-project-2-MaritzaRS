cb469f4ab6524d8e7c6fc4e2b77cc7a35c56b7e2
1861496 <1861496@01-18131.pusd.dom>
2025-03-10 14:00:34 -0700

    I've got approximately 4 personal test cases, 2 form Specs, and 1 from Melody. Notes in Design Specs. We chilling so far. Please let it stay that way.

--- P2_Maritza_Ruiz-Soto/src/p2.java
+++ P2_Maritza_Ruiz-Soto/src/p2.java
@@ -3,7 +3,21 @@ public class p2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		
+		
+		
+		
+		
+		
 		System.out.println("p2");
+		
+		
+		
+		
+		
+		
 	}
 
 }
