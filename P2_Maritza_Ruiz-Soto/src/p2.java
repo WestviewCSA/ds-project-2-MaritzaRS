21b4c106b4f8393be28279b6cf74dc2a9aae8256
1861496 <1861496@01-18131.pusd.dom>
2025-03-28 10:33:20 -0700

    Thank you for teaching me more than I thought I was capable of.

--- P2_Maritza_Ruiz-Soto/src/p2.java
+++ P2_Maritza_Ruiz-Soto/src/p2.java
@@ -8,10 +8,13 @@ public class p2 {
 	private static Queue<Tile> queue;
 	private static Tile wolverine;
 	private static Stack<Tile> visited;
-	private static String[][] maze;
+	private static Character[][] maze;
 	private static Tile diamond;
 	private static int Mrow;
 	private static int Mcol;
+	private static int totalRows;
+	private static int totalCols;
+	//private static int totalRooms;
 	
 	private static Tile north;
 	private static Tile south;
@@ -24,15 +27,27 @@ public class p2 {
 		
 		visited = new Stack<Tile>();
 		
+		//maze = new String[][];	//used to help me determine the char at index since the 
+		maze = new Character[totalRows][totalCols];										//read map method justs prints it
+		System.out.println();
 		readMap("2_FromSpecs");
+		System.out.println(totalRows + " " + totalCols + " ");
+		System.out.println();
 		
-		Tile flop = visited.pop();
+		//Tile flop = visited.pop();
 		
 		visited.push(wolverine);
 		queue.enqueue(wolverine);
 		
 		Mrow = wolverine.getRow();
 		Mcol = wolverine.getCol();
+		
+		north = new Tile(wolverine.getRow()-1, wolverine.getCol(), maze[Mrow-1][Mcol]);
+		south = new Tile(wolverine.getRow()+1, wolverine.getCol(), maze[Mrow+1][Mcol]);
+		east = new Tile(wolverine.getRow(), wolverine.getCol()+1, maze[Mrow][Mcol+1]);
+		west = new Tile(wolverine.getRow(), wolverine.getCol()-1, maze[Mrow][Mcol-1]);
+		
+		queuePath();
 		
 		//System.out.println(wolverine.getType()+" "+ wolverine.getRow() +" "+ wolverine.getCol());
 		
@@ -55,15 +70,15 @@ public class p2 {
 			Scanner scan = new Scanner(file); //taking in the file input
 			
 			int numRows = scan.nextInt();
+			totalRows = numRows;
 			int numCols = scan.nextInt();
+			totalCols = numCols;
 			int numRooms = scan.nextInt();
 			
 			int rowIndex = 0;
 			//MatchResult s = scan.match();
 			System.out.println();
 			//coord.add(numRows + " " + numCols + " " + numRooms);
-			//System.out.println(coord);
-			maze = new String[numRows][numCols];
 			
 			int counter = 0;
 			
@@ -77,20 +92,21 @@ public class p2 {
 					for (int i = 0; i < numCols && i < row.length(); i++) {
 						char el = row.charAt(i); //returns the char
 						//counter++;
+						maze[totalRows][i] = el;
 						Tile obj = new Tile(rowIndex, i, el); //row, col, type
 						//How can I implement the obj into a 2D array?
 //						maze.enqueue(obj);
 						
 						if (el == 'W') {
 							wolverine = obj;
+							
+							
 							//coord2[0][0] = wolverine.toString() + " ";
 							//this is how I locate the wolverine (?)
 						} else if (el == '$') {
 							diamond = obj;
 						}
 						queue.enqueue(obj);
-//						coord.add(el + " " + i + " " + rowIndex + " " + numRooms);
-						maze[rowIndex][i] = el + " ";
 						
 					}
 					
@@ -102,8 +118,7 @@ public class p2 {
 			
 			
 			System.out.println();
-			System.out.println(numRows + " " + numCols + " " + numRooms);
-			System.out.println();
+			
 			System.out.println(maze);
 			//coord2 = new String[counter][4]; 
 			//this will be used to display the visited coordinates
@@ -118,19 +133,55 @@ public class p2 {
 
 	
 	/*
-	 * Used to determine the N S E W
+	 * Used to determine the N S E W, maybe
 	 */
-	public void queuePath() {
+	public static void queuePath() {
 		
 		while (queue.dequeue().getType() != '$') {
 			
-//			for (int row = 0; row < ; row++) {
+			
+			if (north.getRow() < maze.length && north.getRow() >= 0) {
+				queue.enqueue(north);
+				if (north.getType() != '@' && north.getType() != '+') {
+					wolverine = north;
+					north.setType('+');
+				}
+			}
+			
+			if (south.getRow() < maze.length && south.getRow() >= 0) {
+				queue.enqueue(south);
+				if (south.getType() != '@') {
+					wolverine = south;
+					south.setType('+');
+				}
+			}
+			
+			if (east.getRow() < maze.length && east.getRow() >= 0) {
+				queue.enqueue(east);
+				if (east.getType() != '@' && east.getType() != '+') {
+					wolverine = east;
+					east.setType('+');
+				}
+			}
+			
+			if (west.getCol() < maze[totalCols].length && west.getCol() >= 0) {
+				queue.enqueue(west);
+				if (west.getType() != '@' && west.getType() != '+') {
+					wolverine = west;
+					west.setType('+');
+				}
+			}
+			
+//			for (int i = 0; i < queue.size(); i++) {
+//				
+//				//Tile cha = new Tile(wolverine.getRow()+1, wolverine.getCol(), north.getType());
+//				
+//				
 //				
 //			}
 			
 			
 		}
-			
 			
 			
 		
